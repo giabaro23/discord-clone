35cca2c8209c0f999b0dfb752400e07c523d23f4
Bao Nguyen Gia <bao.nguyengia@orientsoftware.com>
2022-02-18 14:39:58 +0700

    third

--- discord-clone-yt/src/App.js
+++ discord-clone-yt/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        
+        <div>aaaa</div>
         <span>
           <span>Learn </span>
           <a
